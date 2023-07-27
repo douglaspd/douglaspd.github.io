7d16f40ffac383d5386b30ccca0e84aa3f8eb1e4
Douglas Domingues <douglaspaivadomingues@gmail.com>
2023-07-27 09:02:11 -0300

    ' testes"

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ const AlterarBackgroundColor = () =>{
 }
 AlterarBackgroundColor();
 
+const buttonBackgroundColor = () => {
+    const buttonColor = document.querySelector('#info-button')
+    const inputColor = document.getElementById ('theme');
+    const backButton = document.querySelector('#option');
+
+    inputColor.addEventListener('input', function() {
+        const selectedColor = inputColor.value;
+        buttonColor.style.backgroundColor=selectedColor;
+        backButton.style.backgroundColor = selectedColor;
+    })
+
+}
+buttonBackgroundColor();
+
+const menuSuspenso = () => {
+
+const infoButton = document.getElementById('info-button');
+const dropDowm = document.querySelector('.dropDown');
+
+infoButton.addEventListener('click', function(event) {
+  this.parentNode.classList.toggle('active');
+  event.stopPropagation();
+    });
+
+    document.addEventListener ('click', function(event){
+        if(!dropDowm.contains(event.target)) {
+            dropDowm.classList.remove('active');
+        }
+    })
+}
+
+menuSuspenso ();
+
+
