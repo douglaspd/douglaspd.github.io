0c3e4b5ce32ea22dfc0a1d9fb62c8eb84b4eb8c5
Douglas de Paiva <douglaspaivadomingues@gmail.com>
2024-11-11 18:02:09 -0300

    ' adicionando a tela de contato'

--- script.js
+++ script.js
@@ -1,87 +1,18 @@
+const currentURL = window.location.href;
 
-const AlterarBackgroundColor = () =>{
+// Abrir o Modal ao clicar no link de contato
+document.getElementById('modal-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('contactModal').style.display = 'none';
+    window.history.back();
+})
 
-    const inputColor = document.getElementById ('theme');
-    const themeElement = document.getElementById ('tema');
-
-    const savedColor = localStorage.getItem('backgroundColor');
-    if(savedColor) {
-        themeElement.style.background = savedColor;
-        inputColor.value = savedColor;
+window.addEventListener('click', function(event) {
+    const modal = document.getElementById('contactModal');
+    const modalContent = document.getElementById('modal-content');
+    if (event.target === modal && event.target !== modalContent) {
+        modal.style.display = 'none';
+        window.history.back();
     }
-
-    inputColor.addEventListener('input', function() {
-        const selectedColor = inputColor.value;
-        themeElement.style.backgroundColor=selectedColor;
-
-        localStorage.setItem('backgroundColor' , selectedColor);
-    });
-}
-AlterarBackgroundColor();
-
-const buttonBackgroundColor = () => {
-    const buttonColor = document.querySelector('#info-button')
-    const inputColor = document.getElementById ('theme');
-    const backButton = document.querySelector('#option');
-
-
-    inputColor.addEventListener('input', function() {
-        const colorSelected = inputColor.value;
-        buttonColor.style.backgroundColor=colorSelected;
-        backButton.style.backgroundColor = colorSelected;
-        localStorage.setItem('backgroundColor', colorSelected);
-    })
-    const savedBack = localStorage.getItem('backgroundColor')
-    if(savedBack) {
-    buttonColor.style.backgroundColor = savedBack;
-    backButton.style.backgroundColor=savedBack;
-};
-}
-
-buttonBackgroundColor();
-
-const menuSuspenso = () => {
-
-const infoButton = document.getElementById('info-button');
-const dropDowm = document.querySelector('.dropdown');
-
-infoButton.addEventListener('click', function(event) {
-  this.parentNode.classList.toggle('active');
-  event.stopPropagation();
-    });
-
-    document.addEventListener ('click', function(event){
-        if(!dropDowm.contains(event.target)) {
-            dropDowm.classList.remove('active');
-        }
-    })
-}
-
-menuSuspenso ();
-
-const darkMode = () =>{
-    const dark = document.getElementById('dark-mode');
-    const body = document.body;
-    const header = document.header;
-    const footer = document.footer;
-
-
-    dark.addEventListener('click' , function(){
-        body.classList.toggle('dark-mode');
-        header.classList.toggle('dark-mode');
-        footer.classList.toggle('dark-mode'); 
-    });
-}
-
-darkMode ()
-
-const removeBebida = (array, remover) => {
-    array.splice(array.indexOf(`${remover}`),1);
-    return array;
-}
-
-
-
-
-
+});
 
